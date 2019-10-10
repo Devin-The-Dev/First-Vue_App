3f9dbb1386bf518f7f3f32ad81bb47763990f38d
Devin Udy <devin.udy@gmail.com>
2019-10-09 18:41:43 -0600

    Added image and link

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'They go on your feet'
+        description: 'They go on your feet',
+        image: './socks.jpg',
+        anchorWord: 'Click Here!',
+        link: 'https://www.freedomskateshop.at/en/accessories/socks/stance-x-deathwish-socks-l-xl?action_ms=1'
     }
 });
