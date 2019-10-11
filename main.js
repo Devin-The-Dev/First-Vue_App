6a0f7a8696e8a4e3a39278b5512825b941e6dee1
Devin Udy <devin.udy@gmail.com>
2019-10-11 17:30:45 -0600

    Added details and variants

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ var app = new Vue({
         anchorWord: 'Click Here!',
         link: 'https://www.freedomskateshop.at/en/accessories/socks/stance-x-deathwish-socks-l-xl?action_ms=1',
         inventory: 10,
-        onSale: true
+        onSale: true,
+        details: ["80% cotton", '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "gray",
+                variantSize: ["Small", "Medium", "Large"]
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantSize: ["Small", "Medium", "Large"]
+            }
+        ]
     }
 });
